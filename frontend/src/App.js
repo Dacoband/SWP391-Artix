119b3c88c885510739da5f3bf92be46595eeea30
TAT610 <tuyettase170234@fpt.edu.vn>
2024-02-22 01:20:09 +0700

    test action icons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,15 +5,16 @@ import Container from './components/Container';
 import Navigation from './components/Navigation';
 import Navigation2 from './components/Navigation2'
 
-
+import TestIcon from './components/TestIcon'
 
 function App() {
   return (
     <div className="App">
      
-      <Navigation/>
-       <Container/>
-     
+      {/* <Navigation/>
+       <Container/> */}
+      
+     <TestIcon/>
      
       
       
---
+++ frontend/src/components/TestIcon.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function TestIcon() {
+  return (
+    <div className='hero'>
+        <button className='btn' >click here </button>
+        
+    </div>
+  )
+}
