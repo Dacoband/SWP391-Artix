94276adca84c3bd8d9213eaa4e8285c08fc53cf6
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2024-02-23 08:51:09 +0700

    Create Folder&File
    for Back-end

---
+++ backend/backend/Controllers/CreatorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    public class CreatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ backend/backend/Entities/Creator.cs
@@ -0,0 +1,14 @@
+﻿namespace backend.Entities
+{
+    public class Creator
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public float Followers { get; set; }
+        public string Phone {  get; set; }
+        public string Address { get; set; }
+        public DateTime? LastLogDate { get; set; }
+        public bool AllowCommission { get; set; }
+
+    }
+}
