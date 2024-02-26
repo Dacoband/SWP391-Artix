6f5f27ed157c7a6e6b50e8cd952be243dc87a706
TAT610 <tuyettase170234@fpt.edu.vn>
2024-02-27 03:51:03 +0700

    HomePage-update

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
 import CarouselTag from'./CarouselTag';
-import RecommendedWorks from './RecommendedWords'
+import RecommendedWorks from './RecommendedWords';
+import RecommendedUsers from './RecommendedUsers';
+import ImgForyou from './ImgForyou';
 export default function HomePage() {
   return (
    
@@ -11,6 +13,7 @@ export default function HomePage() {
         <div className='seemore'>See More</div>
         <CarouselTag/>
       </div>
+
       < div className='recommendedwork'>
         <div className='headrecommended'>
           <h3>Recommended Words</h3>
@@ -18,9 +21,29 @@ export default function HomePage() {
 
        <div className='recommendedimg'>
         <RecommendedWorks/></div>
+      </div>
+
+      <div className='recommendedusers'>
+      <div className='headrecommended'>
+          <h3>Recommended Users</h3>
+          <div className='seemore'>See More</div></div>
+          <div>
+        <RecommendedUsers/></div>
+      </div>
+
+      <div className='Randomimg'>
+      <div className='headrecommended'>
+          <h3>For You</h3>
+          <div className='seemore'>See More</div></div>
+          <div>
+            <ImgForyou/>
+          </div>
+
+
 
 
       </div>
+      
 
 
 
--- frontend/src/components/RecommendedWords.js
+++ frontend/src/components/RecommendedWords.js
@@ -3,7 +3,11 @@ import * as React from 'react';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import { Work } from '../share/ListofWork';
-
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
 export default function RecommendedWords() {
     // Sắp xếp danh sách công việc theo số lượng like giảm dần
   const sortedWork = [...Work].sort((a, b) => b.like - a.like);
@@ -12,19 +16,44 @@ export default function RecommendedWords() {
 
   return (
 
-    <ImageList sx={{ width: 1000, height: 550 ,overflow: 'hidden'}} cols={5} rowHeight={200} >
+    <ImageList sx={{ width: 1100, height: 500 ,overflow: 'hidden'}} cols={5} rowHeight={200} >
       {top10Work.map((work) => (
-        <ImageListItem key={work.id}>
+        <Card className='cardrecommended'>
+        <CardActionArea>
+          <CardMedia  style={{ objectFit: 'cover', width: '100%', height: '180px' }}>
+          <ImageListItem style={{ width:'210px',height: '180px'}} key={work.id}>
           <img
             srcSet={`${work.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
             src={`${work.img}?w=164&h=164&fit=crop&auto=format`}
             alt={work.img}
             loading="lazy"
           />
-          <div>{work.author}</div>
+          
         </ImageListItem>
+          </CardMedia>
+          <CardContent>
+            
+            <div className='userofworks'>{work.author}</div>
+            
+            
+          </CardContent>
+        </CardActionArea>
+      </Card>
+
+
+
+
+       
         
+
+
       ))}
+
+
+
     </ImageList>
+
+
+
   )
 }
