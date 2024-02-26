1ad640fa4d953bfbde21b9ad4fbb7bc81542f595
TAT610 <tuyettase170234@fpt.edu.vn>
2024-02-26 16:53:51 +0700

    tag

--- frontend/src/Javascript/carouseltag.js
+++
@@ -1,9 +0,0 @@
-const carousel = document.querySelector(".ListofTag");
-
-const dragging = (e) => {
-    console.scrollLeft = e.pageX;
-}
-
-if (carousel) {
-    carousel.addEventListener("mousemove", dragging);
-}
--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -3,7 +3,7 @@ import { ListTag } from '../share/ListofTag'
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
-import '../Javascript/carouseltag'
+import CarouselTag from'./CarouselTag';
 export default function HomePage() {
   return (
    
@@ -12,18 +12,8 @@ export default function HomePage() {
     <div className='homepage'>
 
       <div className='carouseltag'>
-        <div className='seemore'>See More:</div>
-        <carouseltag>
-        <ArrowBackIosNewIcon  sx={{ fontSize: 15 }} className='arrow-left'/>
-        <ul className='ListofTag'>
-        {ListTag.map(tag => (
-            <li key={tag.id}>
-              <button className='itemtag'>{tag.nameTag}</button>
-            </li>
-          ))}
-        </ul>
-        <ArrowForwardIosIcon  sx={{ fontSize: 15 }} className='arrow-right'/>
-        </carouseltag>
+        <div className='seemore'>See More</div>
+        <CarouselTag/>
       </div>
 
 
