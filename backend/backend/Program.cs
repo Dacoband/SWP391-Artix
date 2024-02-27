84953dfc10024d87a64cf777e4ccf2cb7afd9200
thucnguyen1234 <127202577+thucnguyen12356666@users.noreply.github.com>
2024-02-27 09:38:38 +0700

    Update Program.cs

--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -14,8 +14,12 @@ public class Program
             c.AddPolicy("AllowOrigin", option => option.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
         });
 
+        //Add Controller 
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+
+
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
         builder.Services.AddDbContext<ApplicationDbContext>();
