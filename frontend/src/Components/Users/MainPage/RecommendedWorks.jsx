e2bdfa0c2baf72dd46b3374be9851c58c7b50fad
Rous1141 <132892586+Rous1141@users.noreply.github.com>
2024-03-02 17:26:50 +0700

    fix FE

--- frontend/src/Components/Users/MainPage/RecommendedWorks.jsx
+++ frontend/src/Components/Users/MainPage/RecommendedWorks.jsx
@@ -3,11 +3,6 @@ import React from 'react';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import { Work } from '../../../share/ListofWork';
-<<<<<<< HEAD
-=======
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
->>>>>>> Volka
 import CardMedia from '@mui/material/CardMedia';
 import { CardActionArea } from '@mui/material';
 export default function RecommendedWords() {
@@ -17,7 +12,6 @@ export default function RecommendedWords() {
   const top10Work = sortedWork.slice(0, 10);
 
   return (
-<<<<<<< HEAD
     <ImageList className='recommendedImages' cols={5} >
       {top10Work.map((work) => (
         <CardActionArea key={work.id}>
@@ -31,28 +25,6 @@ export default function RecommendedWords() {
             />
         </ImageListItem>
         </CardActionArea>
-=======
-
-    <ImageList sx={{ width: 1100, height: 500 ,overflow: 'hidden'}} cols={5} rowHeight={200} >
-      {top10Work.map((work) => (
-        <Card className='cardrecommended'>
-        <CardActionArea>
-          <CardMedia  style={{ objectFit: 'cover', width: '100%', height: '180px' }}>
-          <ImageListItem style={{ width:'210px',height: '180px'}} key={work.id}>
-          <img
-            srcSet={`${work.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-            src={`${work.img}?w=164&h=164&fit=crop&auto=format`}
-            alt={work.img}
-            loading="lazy"
-          />
-        </ImageListItem>
-          </CardMedia>
-          <CardContent>
-            <div className='userofworks'>{work.author}</div>
-          </CardContent>
-        </CardActionArea>
-      </Card>
->>>>>>> Volka
       ))}
     </ImageList>
 
