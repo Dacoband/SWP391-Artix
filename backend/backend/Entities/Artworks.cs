05f872a3bb5d48226984fdf0959f14099a8ae699
thucnguyen1234 <127202577+thucnguyen12356666@users.noreply.github.com>
2024-03-03 19:19:02 +0700

    a

--- backend/backend/Entities/Artworks.cs
+++ backend/backend/Entities/Artworks.cs
@@ -8,7 +8,7 @@ namespace backend.Entities
         [Key]
         public int ArtworkID { get; set; } // PK
         public int CreatorID { get; set; } // FK
-        public int? TagID { get; set; } // FK
+        
         public string  ArtworkName { get; set; } // FK
         public string Description { get; set; }
         public DateTime DateCreated { get; set; }
@@ -16,7 +16,7 @@ namespace backend.Entities
         public bool Purchasable { get; set; }
         public double Price { get; set; }
         public byte[] ImageFile { get; set; } // Thêm cột ImageFile kiểu BLOB
-        
+        public List<ArtworkTag> ArtworkTags { get; set; }
 
     }
 }
--- backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+687a6f885b21e88e566c8373130e04ee86182d36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bf884af32d4a16f55adc350ba5dd06ffc90395f")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
