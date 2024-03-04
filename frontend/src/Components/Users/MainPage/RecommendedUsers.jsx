e4676a6372af6cfebbc0dd11f290385f23e75218
Rous1141 <132892586+Rous1141@users.noreply.github.com>
2024-03-04 21:17:03 +0700

    Revert "test"
    This reverts commit 1ad157f9cd235b04c63d9d21d11c6b2512c8005d.

--- frontend/src/Components/Users/MainPage/RecommendedUsers.jsx
+++ frontend/src/Components/Users/MainPage/RecommendedUsers.jsx
@@ -29,8 +29,8 @@ function RecommendedUsers() {
                      <div className="inforuser">
                   <div className="imguser">
                     <img
-                      srcSet={`${user.avatar}`}
-                      src={`${user.avatar}`}
+                      srcSet={`${user.avatar}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                      src={`${user.avatar}?w=164&h=164&fit=crop&auto=format`}
                       alt={user.avatar}
                       loading="lazy" className="imgu"
                     />
