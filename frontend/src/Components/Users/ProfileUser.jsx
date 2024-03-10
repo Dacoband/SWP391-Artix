d0ea9e4b9b0324212137e704bb58f9dcc820aae4
Rous1141 <132892586+Rous1141@users.noreply.github.com>
2024-03-10 20:58:58 +0700

    form

--- frontend/src/Components/Users/ProfileUser.jsx
+++ frontend/src/Components/Users/ProfileUser.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Work } from '../../share/ListofWork';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
@@ -25,6 +25,7 @@ import Avatar from '@mui/material/Avatar';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
 import IconButton from '@mui/material/IconButton';
 import InfoIcon from '@mui/icons-material/Info';
+import { ThemeContext } from '../Themes/ThemeProvider.tsx';
 function CustomTabPanel(props) {
   const { children, value, index, ...other } = props;
   return (
@@ -62,7 +63,7 @@ export default function ProfileUser() {
   const [isFollowing, setIsFollowing] = useState(false)
   let { id } = useParams()
   const selectedUser = ListofUsers.find(user => user.id === parseInt(id));
-
+  const { theme } = useContext(ThemeContext)
   const handleClick = () => {
     setIsFollowing(!isFollowing)
   }
@@ -71,12 +72,6 @@ export default function ProfileUser() {
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
-
-  if (!selectedUser) {
-    return <div>Error</div>;
-  }
-  console.log(selectedUser)
-
   return (
 
     <div className=''>
@@ -90,7 +85,6 @@ export default function ProfileUser() {
             <Button className='button-edit-background' style={{ color: '#04a1fd', backgroundColor: '#1A1A2E', borderRadius: '10px', fontSize: '14px', top: '80%', left: '85%' }}><CameraAltIcon /> Edit Cover Image</Button>
           </div>
           <CardContent className='infouser1'>
-
             <div className='infousername'>
               <div className='avataruser' >
                 <img src={selectedUser.avatar} />
@@ -127,135 +121,125 @@ export default function ProfileUser() {
 
             </div>
           </CardContent>
-
         </Card>
 
-
       </div>
-      <div className='inforuser2'>
-        <Box sx={{ width: '100%' }}>
-
-          <Box sx={{ borderBottom: 1, borderColor: 'divider' }} className='navofuser'>
-            <div className='navuser'>
-              <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-                <Tab label="Home" {...a11yProps(0)} />
-                <Tab label="Shop" {...a11yProps(1)} />
-                <Tab label="Favourites" {...a11yProps(2)} />
-              </Tabs>
-            </div>
-            <div className='buttonSubcribe'>
-              <Button variant="contained" href="#contained-buttons"> <ShoppingBagIcon style={{ marginRight: '5px' }} />  Subcribe</Button>
-              <Button variant="contained" href="#contained-buttons" style={{ marginLeft: '20px' }}>Report</Button>
-            </div>
-          </Box>
-
-
-
-          <CustomTabPanel value={value} index={0} >
-            <div className='tabhome'>
-              <div className='biouser'>
-                <Box
-                  // height= {150}
-                  width={350}
-                  my={4}
-
-                  gap={4}
-                  p={2}
-
-
-                  style={{
-                    border: '2px solid grey',
-                    position: 'sticky', // this will make it sticky
-                    top: 0, // this defines the top position when it's sticky
-                    zIndex: 10 // you may want to add a zIndex to ensure it stacks on top of other contents
-
-
-                  }} className='boxintroduct'
-                >
-                  <h2 className='headintroduct'>About {selectedUser.User}:</h2>
-
-                  <div className='contentintroduct'><CakeIcon className='iconintroduct' />Birday: {selectedUser.birday} </div>
-                  <div className='contentintroduct'><RoomIcon className='iconintroduct' />Location: {selectedUser.location}</div>
-                  <div className='contentintroduct'><EmailIcon className='iconintroduct' />Email: {selectedUser.email} </div>
-                  <div className='contentintroduct'><PhoneIcon className='iconintroduct' />Phone: {selectedUser.Phone}</div>
-                  <div className='contentintroduct'> <AutoAwesomeIcon className='iconintroduct' />My Bio: {selectedUser.bio}  </div>
-
-                </Box></div>
-              <div className='workofuser'>
-                <div className='head-workofuser'>
-                  <h2> My Works:</h2></div>
-                <Box sx={{ width: 1000, height: 450, overflow: 'visible' }}>
-                  <ImageList variant="masonry" cols={3} gap={8}>
-                    {Work.map((work) => (
-                      <ImageListItem key={work.id}>
-                        <img
-                          srcSet={`${work.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                          src={`${work.img}?w=248&fit=crop&auto=format`}
-                          alt={work.title}
-                          loading="lazy"
-                        />
-                      </ImageListItem>
-                    ))}
-                  </ImageList>
-                </Box></div>
-            </div>
+      <div className='tabsBackground' style={{ backgroundColor: theme.backgroundColor }} >
+        <div className='inforuser2'>
+          <Box sx={{ width: '100%' }}>
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }} className='navofuser'>
+              <div className='navuser'>
+                <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+                  <Tab label="Home" {...a11yProps(0)} />
+                  <Tab label="Shop" {...a11yProps(1)} />
+                  <Tab label="Favourites" {...a11yProps(2)} />
+                </Tabs>
+              </div>
+              <div className='buttonSubcribe'>
+                <Button variant="contained" href="#contained-buttons"> <ShoppingBagIcon style={{ marginRight: '5px' }} />  Subcribe</Button>
+                <Button variant="contained" href="#contained-buttons" style={{ marginLeft: '20px' }}>Report</Button>
+              </div>
+            </Box>
+            <CustomTabPanel value={value} index={0} >
+              <div className='tabhome'>
+                <div className='biouser'>
+                  <Box
+                    // height= {150}
+                    width={350}
+                    my={4}
+                    gap={4}
+                    p={2}
+                    style={{
+                      border: '2px solid grey',
+                      top: 0, // this defines the top position when it's sticky
+                      zIndex: 10 // you may want to add a zIndex to ensure it stacks on top of other contents
+                    }} className='boxintroduct'
+                  >
+                    <h2 className='headintroduct'>About {selectedUser.User}:</h2>
+                    <div className='contentintroduct'><CakeIcon className='iconintroduct' />Birday: {selectedUser.birday} </div>
+                    <div className='contentintroduct'><RoomIcon className='iconintroduct' />Location: {selectedUser.location}</div>
+                    <div className='contentintroduct'><EmailIcon className='iconintroduct' />Email: {selectedUser.email} </div>
+                    <div className='contentintroduct'><PhoneIcon className='iconintroduct' />Phone: {selectedUser.Phone}</div>
+                    <div className='contentintroduct'> <AutoAwesomeIcon className='iconintroduct' />My Bio: {selectedUser.bio}  </div>
+                  </Box></div>
+                <div className='workofuser'>
+                  <div className='head-workofuser'>
+                    <h2> My Works:</h2>
+                    <Box>
+                      <ImageList variant="masonry" cols={3} gap={8}>
+                        {Work.map((work) => (
+                          <ImageListItem key={work.id}>
+                            <img
+                              srcSet={`${work.img}`}
+                              src={`${work.img}`}
+                              alt={work.title}
+                              loading="lazy"
+                            />
+                          </ImageListItem>
+                        ))}
+                      </ImageList>
+                    </Box>
+                  </div>
+                </div>
+              </div>
 
 
-          </CustomTabPanel>
+            </CustomTabPanel>
 
-          <CustomTabPanel value={value} index={1} className='tabshop'>
-            {/* <ImageList sx={{ width:1200 , height: 450 ,overflow: 'hidden'}} cols={5} rowHeight={210}> */}
-            <ImageList sx={{ width: 1200, height: 'auto', overflow: 'visible' }} cols={4}>
-              {/* <ImageListItem key="Subheader" cols={2}>
+            <CustomTabPanel value={value} index={1} className='tabshop'>
+              {/* <ImageList sx={{ width:1200 , height: 450 ,overflow: 'hidden'}} cols={5} rowHeight={210}> */}
+              <ImageList sx={{ width: 1200, height: 'auto', overflow: 'visible' }} cols={4}>
+                {/* <ImageListItem key="Subheader" cols={2}>
         <ListSubheader component="div">December</ListSubheader>
-      </ImageListItem> */}
-              {Work.map((work) => (
-                <ImageListItem key={work.id}>
-                  <img
-                    srcSet={`${work.img}?w=240&fit=crop&auto=format&dpr=2 2x`}
-                    src={`${work.img}?w=240&fit=crop&auto=format`}
-                    alt={work.title}
-                    loading="lazy"
-                    style={{ height: '200px' }}
-                  />
-                  <ImageListItemBar
-                    title={work.price}
-                    subtitle={work.author}
-                    actionIcon={
-                      <IconButton
-                        sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
-                        aria-label={`info about ${work.author}`}
-                      >
-                        <InfoIcon />
-                      </IconButton>
-                    }
-                  />
-                </ImageListItem>
-              ))}
-            </ImageList>
-          </CustomTabPanel>
-
-
-          <CustomTabPanel value={value} index={2} className='tabfavourites'>
-
-            <Box sx={{ width: 1200, height: 450, overflow: 'visible' }}>
-              <ImageList variant="masonry" cols={4} gap={8}>
+        </ImageListItem> */}
                 {Work.map((work) => (
                   <ImageListItem key={work.id}>
                     <img
-                      srcSet={`${work.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                      src={`${work.img}?w=248&fit=crop&auto=format`}
+                      srcSet={`${work.img}?w=240&fit=crop&auto=format&dpr=2 2x`}
+                      src={`${work.img}?w=240&fit=crop&auto=format`}
                       alt={work.title}
                       loading="lazy"
+                      style={{ height: '200px' }}
+                    />
+                    <ImageListItemBar
+                      title={work.price}
+                      subtitle={work.author}
+                      actionIcon={
+                        <IconButton
+                          sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                          aria-label={`info about ${work.author}`}
+                        >
+                          <InfoIcon />
+                        </IconButton>
+                      }
                     />
                   </ImageListItem>
                 ))}
               </ImageList>
-            </Box>
-          </CustomTabPanel>
+            </CustomTabPanel>
 
-        </Box>
 
+            <CustomTabPanel value={value} index={2} className='tabfavourites'>
+
+              <Box sx={{ width: 1200, height: 450, overflow: 'visible' }}>
+                <ImageList variant="masonry" cols={4} gap={8}>
+                  {Work.map((work) => (
+                    <ImageListItem key={work.id}>
+                      <img
+                        srcSet={`${work.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                        src={`${work.img}?w=248&fit=crop&auto=format`}
+                        alt={work.title}
+                        loading="lazy"
+                      />
+                    </ImageListItem>
+                  ))}
+                </ImageList>
+              </Box>
+            </CustomTabPanel>
+
+          </Box>
+
+        </div>
       </div>
     </div>
   );
