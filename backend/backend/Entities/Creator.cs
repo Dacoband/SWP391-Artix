d0b6739e21362145dd7624e32fd36bff7ff79618
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2024-03-13 14:36:31 +0700

    update creator entiti

--- backend/backend/Entities/Creator.cs
+++ backend/backend/Entities/Creator.cs
@@ -11,14 +11,14 @@ namespace backend.Entities
         public int? PaypalAccountID { get; set; } // FK
         public string UserName { get; set; }
         public int? FollowerID { get; set; } // FK
-        public string  ProfilePicture { get; set; } // Có thể 
+        public string?  ProfilePicture { get; set; } // Có thể 
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Address { get; set; }
         public string Phone { get; set; }
         public DateTime LastLogDate { get; set; }
         public bool AllowCommission { get; set; }
-        public string Biography { get; set; }
+        public string? Biography { get; set; }
 
 
 
