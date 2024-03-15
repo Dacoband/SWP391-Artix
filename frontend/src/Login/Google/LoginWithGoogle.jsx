30f0aecebffb310c765484f742d3d9c6cb08f9ab
Rous1141 <132892586+Rous1141@users.noreply.github.com>
2024-03-15 23:37:34 +0700

    Login GG

--- frontend/src/Login/Google/LoginWithGoogle.jsx
+++ frontend/src/Login/Google/LoginWithGoogle.jsx
@@ -1,4 +1,4 @@
-import React, { } from 'react'
+import React, { useState } from 'react'
 import { useGoogleLogin } from '@react-oauth/google'
 import GoogleIcon from '@mui/icons-material/Google';
 import Button from '@mui/material/Button';
@@ -6,9 +6,10 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom'
 import { useAuth } from '../../Components/AuthenContext.tsx';
 export default function LoginWithGoogle({ disableOutsideClick, handleClick }) {
-  const accounturl = 'https://localhost:7233/api/Account/email/'
+  const accounturl = 'https://localhost:7233/api/Account'
   const creatorurl = 'https://localhost:7233/api/Creator/'
   const roleurl = 'https://localhost:7233/api/Role/'
+
   const { storeUserData } = useAuth();
   //Call the custom hook to store user login information
   const navigate = useNavigate();
@@ -30,24 +31,27 @@ export default function LoginWithGoogle({ disableOutsideClick, handleClick }) {
       //Using Axios to fetch API from Google
       //Async await to synchonize fetching data
       //TODO: Remove console.log when finish testing
-      await axios
-        .get(googleAPI, { headers: { Authorization: `Bearer ${token}` } })
-        .then(response => response.data)
-        .then(user => {
-          console.log(user);
-          const accountResponse = axios.get(accounturl+user.email)
-          const account = accountResponse.data;
-          const creatorResponse = axios.get(creatorurl + account.accountID)
-          const creator = creatorResponse.data;
-          const userroleResponse = axios.get(roleurl + account.roleID);
-          const userrole = userroleResponse.data;
-          //Store the user role in sesison
-          sessionStorage.setItem('userRole', userrole.roleName);
-          storeUserData(creator); //Store user data from Google to session storage
-          window.dispatchEvent(new Event('userLoggedIn'));
-        })
-        .then(redirect)
-        .catch(error => console.log(error))
+     const ggAccount = await axios.get(googleAPI, { headers: { Authorization: `Bearer ${token}` } }).then(response => response.data)
+     const listOfAccounts = await axios.get(accounturl).then(response => response.data)
+     const foundAccount = listOfAccounts.find((account) => account.email === ggAccount.email);
+      if (foundAccount) {
+         //Get the user roles
+      const userroleResponse = await axios.get(roleurl+foundAccount.roleID);
+      const userrole = userroleResponse.data;
+      //Store the user role in sesison
+      sessionStorage.setItem('userRole', userrole.roleName);
+       // Once the user is verified, get additional user data.
+      const creatorResponse = await axios.get(creatorurl + foundAccount.accountID);
+      const creatorData = creatorResponse.data;
+      storeUserData(creatorData);
+        window.dispatchEvent(new Event('userLoggedIn'));
+        if (userrole.roleName === "AD") {
+          navigate('/admin');
+        } else {
+          navigate('/characters');
+        }
+      }
+
     },
     onError: error => { console.log(error) }
   }, [])
