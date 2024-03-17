af01c893ff30935a526f3d1ab1644168a0222a11
TAT610 <tuyettase170234@fpt.edu.vn>
2024-03-18 00:09:35 +0700

    1

--- backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+867d72280a0359760b16a4e81c56b857056c8be2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dd426af7dbce39be1f4e3ba8c9360b315bcec1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
