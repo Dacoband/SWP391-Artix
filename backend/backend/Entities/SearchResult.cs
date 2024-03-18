fd4b0a732d1cfd8531a060b644c7384050b5eb5c
TAT610 <tuyettase170234@fpt.edu.vn>
2024-03-18 16:19:26 +0700

    1

--- backend/backend/Entities/SearchResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace backend.Entities
-{
-    public class SearchResult
-    {
-
-        public List<Creator> Creators { get; set; }
-        public List<Artworks> Artworks { get; set; }
-    }
-}
