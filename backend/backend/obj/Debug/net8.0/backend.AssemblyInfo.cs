0f77af3266617601d835e30c6b050bc2fbcecd8b
Rous1141 <132892586+Rous1141@users.noreply.github.com>
2024-03-20 10:43:44 +0700

    Revert "be"
    This reverts commit 694b76c59c63a94b58a28fbda1b2abc34a5044b7, reversing
    changes made to 0088f5ada2fafaae6912a9ae878f967acef3ee58.

--- backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffd9f77b73ddcfa7759c4f2ec5d16d32e4be2247")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcf735ec3d226a8802df643d92f2dca4b26995e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
