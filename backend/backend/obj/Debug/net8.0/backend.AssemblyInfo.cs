ba3a04f03c73b02ec404708dcc569af4667cfcbc
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2024-03-22 07:47:39 +0700

    Add Task

--- backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04cfbc4b8a817c54d2d7b3de7d0a68ce62e06d0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f47c6216b5ededeac03d09408e56cbf5b5ac21d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
