e4db892b0c1f2c4b303575d3838f8b32b690ad8d
thucnguyen1234 <127202577+thucnguyen12356666@users.noreply.github.com>
2024-03-22 15:14:38 +0700

    1

--- backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -14,23 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77114b88a96a75e47390ad66a54aa1941a3ed464")]
-=======
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c11078e1765264bf6a016f47003e91f1a13b9ce2")]
-=======
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4c583c69653043daa8383821ad70ff701d1aa78")]
-=======
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eadb6a6dc97c46b4def4ccc4071bf00e27b5ea02")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08ed222729805495e143462b9ca85a159d835ac6")]
->>>>>>> d4c583c69653043daa8383821ad70ff701d1aa78
->>>>>>> 4574d2145ab9ad9a4af352b925abd7925d81a96c
->>>>>>> 77114b88a96a75e47390ad66a54aa1941a3ed464
->>>>>>> 50d04100341164ff20789eb75ddc0653cc2e8d86
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78680e167bc2aeec3be454d5327f7b681355c6a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
