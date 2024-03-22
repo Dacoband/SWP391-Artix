d6223072799f7625efd0befdcb9da40bc761753e
Huỳnh Thiện Nhân <bolicious123@gmail.com>
2024-03-22 15:21:37 +0700

    Total Like 7 days

--- backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78680e167bc2aeec3be454d5327f7b681355c6a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00c70a63f8ccef207e645932e645b3c4b05057ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
